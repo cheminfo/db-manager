e6a790f539ad99f1f5ac9c3ccbd31f6f7386ff64
Michaël Zasso <mic.besace@gmail.com>
2015-01-05 15:56:13 +0100

    change main file in package.json

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+// Check support for ES6 generators
 var generator = 'function*test(){}';
 try {
     eval(generator);
 } catch (e) {
     if (e instanceof SyntaxError) {
         console.error('Generators not supported. This script must be run with the --harmony flag.');
-        process.exit(1);
+    } else {
+        console.error('Unexpected error', e);
     }
+    process.exit(1);
 }
 
 var program = require('commander');
@@ -14,16 +17,19 @@ program
     .option('-d, --debug', 'Enable debug messages')
     .parse(process.argv);
 
+// If debug mode is on, add db-manager to the ENV variable to enable the debug lib
 if (program.debug) {
     process.env.DEBUG = (process.env.DEBUG || '') + ',db-manager:*';
 } else {
     console.log('Starting...');
 }
 
+// Disable ugly mongoose warning
+// TODO remove when mongoose 4.0.0 is out
 process.env.MONGOOSE_DISABLE_STABILITY_WARNING = true;
 
-var DBManager = require('./');
-var manager = new DBManager('/home/mzasso/git/cheminfo/db-manager/src');
+var DBManager = require('./src/index');
+var manager = new DBManager();
 
 manager.start().then(function () {
     if (!program.debug) {
